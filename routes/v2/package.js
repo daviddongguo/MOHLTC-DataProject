27fad9ef6859d216dad9587c4d29a7ec3d12392f
daviddongguo <david.dong.guo@gmail.com>
2019-08-26 17:55:20 -0400

    [git] reorder the package router

--- routes/v2/package.js
+++ routes/v2/package.js
@@ -6,20 +6,16 @@ const {
     adminDeletePackage, userSaveWorkbook
 } = packageController;
 
-router.get('/api/v2/admin/packages/:name', adminGetPackage);
-
-router.get('/api/v2/packages/:name', userGetPackage);
-
-router.get('/api/v2/packages/:packageName/:name', userGetWorkbook);
-
-router.put('/api/v2/packages/:packageName/:name', userSaveWorkbook);
 
 router.get('/api/v2/admin/packages', adminGetAllPackages);
+router.post('/api/v2/admin/packages', adminCreatePackage);
+router.get('/api/v2/admin/packages/:name', adminGetPackage);
+router.delete('/api/v2/admin/packages/:name', adminDeletePackage);
 
 router.get('/api/v2/packages', userGetAllPackages);
+router.get('/api/v2/packages/:name', userGetPackage);
+router.get('/api/v2/packages/:packageName/:name', userGetWorkbook);
+router.put('/api/v2/packages/:packageName/:name', userSaveWorkbook);
 
-router.post('/api/v2/admin/packages', adminCreatePackage);
-
-router.delete('/api/v2/admin/packages/:name', adminDeletePackage);
 
 module.exports = router;
