68cfea97c464576c84b07dd657347a80fae5cef6
daviddongguo <david.dong.guo@gmail.com>
2020-10-13 12:47:07 -0400

    Update 2020-10-13

--- models/workbook/attribute.js
+++ models/workbook/attribute.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-module.exports = mongoose.model('Attribute',
-    new mongoose.Schema({
-        groupNumber: {type: Number, required: true},
-        name: {type: String, required: true},
-        id: {type: Number, required: true, unique: true},
-        description: String,
-        groups: [ObjectId],
-    }));
+module.exports = mongoose.model(
+  "Attribute",
+  new mongoose.Schema({
+    groupNumber: { type: Number, required: true },
+    name: { type: String, required: true },
+    id: { type: Number, fkequired: true, unique: true },
+    description: String,
+    groups: [ObjectId]
+  })
+);
