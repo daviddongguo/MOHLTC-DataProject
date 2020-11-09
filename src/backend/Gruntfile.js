074e8494cd30ed955c92d75852fbba50e63dc254
daviddongguo <david.dong.guo@gmail.com>
2020-11-09 16:21:36 -0500

    Add gruntfile Add a defualt task

---
+++ src/backend/Gruntfile.js
@@ -0,0 +1,83 @@
+module.exports = function (grunt) {
+	dd;
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-run');
+	grunt.loadNpmTasks('grunt-env');
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		copy: {
+			main: {
+				files: [
+					{
+						expand: true,
+						src: [
+							'public/**',
+							'config/**',
+							'controller/**',
+							'documents/**',
+							'models/**',
+							'routes/**',
+							'views/**',
+							'test/**',
+							'mochawesome-report/**',
+							'app.js',
+							'Gruntfile.js',
+							'package.json',
+							'yarn.lock',
+						],
+						dest: 'build/zip/',
+					},
+					{
+						expand: true,
+						cwd: '../Data-Project-Config/',
+						src: [
+							'.ebextensions/**', // amazon beanstalk configs
+							'config/**',
+						],
+						dest: 'build/zip/',
+					},
+					{
+						expand: true,
+						cwd: './frontend/build/',
+						src: ['**'],
+						dest: 'build/zip/public/react',
+					},
+				],
+			},
+		},
+
+		compress: {
+			main: {
+				options: {
+					archive: 'release-beta.zip',
+				},
+				expand: true,
+				cwd: 'build/zip',
+				src: ['**/*', '.*/*'],
+				dest: './',
+			},
+		},
+
+		clean: ['build', 'public/dist', 'release-beta.zip'],
+
+		run: {
+			report: {
+				exec:
+					'"./node_modules/.bin/mocha" ./test/main.js ./test/ --recursive --exit --check-leaks -R mochawesome',
+			},
+		},
+	});
+
+	// mkdir for zip archive
+	grunt.registerTask('mkdir', function () {
+		grunt.file.mkdir('build/zip/temp');
+		grunt.file.mkdir('build/zip/uploads');
+		grunt.file.mkdir('build/zip/public/react');
+	});
+
+	grunt.registerTask('default', 'Executed default task', function () {});
+};
