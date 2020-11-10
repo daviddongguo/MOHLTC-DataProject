fec123cda01cde4ca2561b4b67b6e138c6514a80
daviddongguo <david.dong.guo@gmail.com>
2020-11-10 09:47:10 -0500

    Restore cleverMongoDb

---
+++ src/backend/config/cleverMongoDb.js
@@ -0,0 +1,5 @@
+module.exports = {
+	database:
+		'mongodb://ufj9soydq4c31zzk4cbz:9AIrwtTT4GtEmowt4c8V@baiduixhhqqufam-mongodb.services.clever-cloud.com:27017/baiduixhhqqufam',
+	secondDatabase: '',
+};
