fa466d7d8eb6b67c0268e00e7edb34e2f818ccff
daviddongguo <david.dong.guo@gmail.com>
2020-11-10 12:28:32 -0500

    Add a test link for backend api

--- src/backend/routes/index.js
+++ src/backend/routes/index.js
@@ -5,19 +5,29 @@ const setupController = require('../controller/setup');
 
 router.post('/api/setup', setupController.firstTimeSetup);
 
+router.get('/test', function (req, res) {
+	const currentTimeStr = new Date()
+		.toISOString()
+		.replace(/T/, ' ')
+		.replace(/\..+/, '');
+	return res
+		.status(200)
+		.json({success: true, message: 'working, now is : ' + currentTimeStr});
+});
+
 router.get('/', function (req, res) {
-    if (config.firstTimeRun) {
-        if (process.env.NODE_ENV === 'production') {
-            return res.redirect('/react/#/setup');
-        } else {
-            return res.redirect('http://localhost:3003/#/setup');
-        }
-    }
-    if (process.env.NODE_ENV === 'production') {
-        return res.redirect('/react');
-    } else {
-        return res.redirect('http://localhost:3003/');
-    }
+	if (config.firstTimeRun) {
+		if (process.env.NODE_ENV === 'production') {
+			return res.redirect('/react/#/setup');
+		} else {
+			return res.redirect('http://localhost:3003/#/setup');
+		}
+	}
+	if (process.env.NODE_ENV === 'production') {
+		return res.redirect('/react');
+	} else {
+		return res.redirect('http://localhost:3003/');
+	}
 });
 
 module.exports = router;
