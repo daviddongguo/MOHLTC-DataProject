d530c0bd8bd0f2a4890a60934144ee20be2720de
daviddongguo <david.dong.guo@gmail.com>
2020-11-27 16:59:33 -0500

    Update .travis

--- src/backend/config/cleverMongoDb.js
+++ src/backend/config/cleverMongoDb.js
@@ -1,6 +1,4 @@
 module.exports = {
-	database:
-		'mongodb://ufj9soydq4c31zzk4cbz:9AIrwtTT4GtEmowt4c8V@baiduixhhqqufam-mongodb.services.clever-cloud.com:27017/baiduixhhqqufam',
+	database: process.env.MONGODB_CONNECTION_STRING,
 	secondDatabase: '',
-	thereeDatabase: '',
 };
